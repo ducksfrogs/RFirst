5432fd7af1c38678c3207b276edd48737b201234
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-02 11:37:41 +0900

    min

--- minR/base_stat.R
+++ minR/base_stat.R
@@ -24,3 +24,20 @@ tipsummary
 
 ggplot(tipsummary, aes(x=tip.mean, y=sex)) + geom_point() +
   geom_errorbar(aes(xmin=Lower, xmax=Upper), height= .2)
+
+
+tipAnova <- aov(tip ~ day -1, tips)
+tipAnova$coefficients
+tipIntercept <- aov(tip ~ day, tips)
+tipIntercept$coefficients
+summary(tipAnova)
+
+require(tidyverse)
+require(plyr)
+tipsByDay <- ddply(tips, "day", summarise,
+                   tip.mean = mean(tip), tip.sd = sd(tip),
+                   Length = NROW(tip),
+                   tfrac = qt(p=.90, df = Length -1),
+                   Lower = tip.mean - tfrac*tip.sd/sqrt(Length),
+                   Upper = tip.mean + tfrac*tip.sd/sqrt(Length))
+tipsByDay
